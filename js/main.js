48707b66ad3e773bfc0cf0df225cfa13b98a21a3
bhargavmugada <78066587+bhargavmugada@users.noreply.github.com>
2021-02-01 15:46:27 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch("../data/data.json").then(response=>{
+fetch("https://bhargavmugada.github.io/ecommers/data/data.json").then(response=>{
 return response.json()
 }).then(data=>{
 displayData(data)
